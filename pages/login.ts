e6d2cd860c9d638591814f52f35d8fe3b9fd42de
lzanchin <leandro.zanchin@gmail.com>
2024-05-24 13:28:15 +1000

    Login page and initial test setup

--- pages/login.ts
+++ pages/login.ts
@@ -1,5 +1,41 @@
 import { Page, Locator } from '@playwright/test';
 
 export class LoginPage {
+    readonly page: Page;
+    readonly $usernameInput: Locator;
+    readonly $passwordInput: Locator;
+    readonly $loginButton: Locator;    
+
+    constructor(page: Page) {
+        this.page = page;
+        this.$usernameInput = page.getByPlaceholder('name@host.com');
+        this.$passwordInput = page.getByPlaceholder('Password');
+        this.$loginButton = page.getByRole('button', { name: 'Sign in' });        
+    };
+
+    // elements methods and actions
+
+    async open(url: string) {        
+        await this.page.goto(url);
+    };
+
+    async fillUsername(username: string) {
+        await this.$usernameInput.fill(username);
+    };
+
+    async fillPassword(password: string) {
+        await this.$passwordInput.fill(password);
+    };
+
+    async clickLoginButton() {
+        await this.$loginButton.click();
+    };
     
-}
+    // Page Workflows
+
+    async login(username: string, password: string) {
+        await this.fillUsername(username);
+        await this.fillPassword(password);
+        await this.clickLoginButton();
+    };    
+};
--- tests/leonardo.spec.ts
+++ tests/leonardo.spec.ts
@@ -8,3 +8,14 @@ const password = process.env.PASSWORD as string;
 const username = process.env.USERNAME as string;
 const url = process.env.WEB_URL as string;
 
+test.describe('Login and generate one image based on a prompt', () => {
+    test('It should login and generate one image', async ({ page }) => {
+        test.slow();
+        let loginPage = new LoginPage(page);
+        
+        await test.step('Login to Leonardo', async () => {
+            await loginPage.open(url);
+            await loginPage.login(username, password);
+        });
+    });    
+});
